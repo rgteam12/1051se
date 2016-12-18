551b497852109dd19c79f9f91d82b313020889d4
a13777666 <a13777666@gmail.com>
2016-12-19 01:55:39 +0800

    Update showoven.php

--- team8/showoven.php
+++ team8/showoven.php
@@ -32,8 +32,8 @@ function clickfunction(ovenid,uoven){
 		document.getElementById(uoven.id).disabled=true;
 	}
 	else if(v=="完成囉"){
-		$("#div010").fadeIn("slow");
-		DIV="div011";
+		//$("#div010").fadeIn("slow");
+		//DIV="div011";
 		$.ajax({
 		url: 'ovenfindone.php',
 		datatype:'html',
